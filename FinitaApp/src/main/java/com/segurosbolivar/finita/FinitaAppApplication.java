b9780dc576187c37e92658626142d572e1d0a3c1
Carlos Alegria <calegria@asesoftware.com>
2020-08-12 19:17:51 -0500

    COMMIT INICIAL

---
+++ FinitaApp/src/main/java/com/segurosbolivar/finita/FinitaAppApplication.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.finita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinitaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinitaAppApplication.class, args);
+	}
+
+}
---
+++ FinitaApp/src/test/java/com/segurosbolivar/finita/FinitaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.finita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinitaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
